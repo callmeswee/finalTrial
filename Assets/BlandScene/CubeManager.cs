886377d6eb6957a598f0df4d6954273b23ed0574
callmeswee <sj2468@nyu.edu>
2019-05-21 09:57:03 +0200

    rePutting Raw Gender

---
+++ Assets/BlandScene/CubeManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CubeManager : MonoBehaviour
+{
+
+    public GameObject instructionText;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnEndEdit(string theInput){
+        Debug.Log(theInput);
+
+        if(theInput == "hello"){
+            
+            GetComponent<Animator>().Play("CubeScale");
+
+        }else if(theInput == "goodbye"){
+            
+            GetComponent<Animator>().Play("CubeSpinning"); 
+
+        }else{
+            Debug.Log("sorry");
+
+            instructionText.GetComponent<Text>().text = "Sorry, try again!";
+        }
+    }
+
+    public void OnInput(){
+        
+    }
+
+    public void OnClick()
+    {
+        
+
+    }
+
+}
---
+++ Assets/MainManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ReceivedTouch(Vector3 position, Quaternion rotation){
+        // --------------------
+        // THIS IS WHERE YOU WRITE THE CODE WHEN THE USER TOUCHES A BLUE PLANE
+        // --------------------
+    }
+
+    public void TouchedObject(GameObject obj){
+        // --------------------
+        // THIS IS WHERE YOU WRITE THE CODE WHEN THE USER TOUCHES AN OBJECT
+        // --------------------
+    }
+}
