dbefac4e4ade36c16b816c260202101bd89a674b
callmeswee <sj2468@nyu.edu>
2019-05-21 10:36:44 +0200

    Added Parented object with one colider and other test

---
+++ Assets/rightFadeStarter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rightFadeStarter : MonoBehaviour
+{
+
+    public GameObject rightSquare;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rightSquare.SetActive(false);
+    }
+
+    // checking triger enter 
+    private void OnTriggerEnter(Collider other)
+    {
+        rightSquare.SetActive(true);
+        StartCoroutine("WaitTime");
+    }
+
+    IEnumerator WaitTime()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(rightSquare);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
